d2a84fc4b6571536162b9e9e9c97d0d1d3cbf55c
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-01 19:33:22 +0300

    delet html

--- webhtml/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuLinks = document.querySelectorAll("header nav ul li a");
-    menuLinks.forEach((link, index) => {
-        setTimeout(() => {
-            link.style.opacity = 1;
-        }, index * 300);
-    });
-});
