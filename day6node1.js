e1699e6cd7b3ee549a2d6684328f9a0e901c788e
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-01 19:20:43 +0300

    Update day6node1.js

--- day6node1.js
+++ day6node1.js
@@ -1,5 +1,25 @@
 const fs = require('fs');
 
+
+fs.readFile('day6node.md', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    console.log(data);
+});
+
+
+console.log(
+    setTimeout(() => {
+        console.log("This is a timeout FOR end of readfile");
+    }, 1000), "we now read the file asyncronously");
+
+
+const data = fs.readFileSync('day6node.md', 'utf8');
+console.log(data);
+
 fs.writeFile('day6node.txt', 'Hello Write file asyncronously', (err) => {
     if (err) {
         console.error(err);
