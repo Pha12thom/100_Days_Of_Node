315264882dfc28345ba827006bbd63fd2c067807
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-01 19:29:07 +0300

    commit all file modules

--- day6node1.js
+++ day6node1.js
@@ -1,5 +1,25 @@
 const fs = require('fs');
 
+
+fs.readFile('day6node.md', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    console.log(data);
+});
+
+
+console.log(
+    setTimeout(() => {
+        console.log("This is a timeout FOR end of readfile");
+    }, 1000), "we now read the file asyncronously");
+
+
+const data = fs.readFileSync('day6node.md', 'utf8');
+console.log(data);
+
 fs.writeFile('day6node.txt', 'Hello Write file asyncronously', (err) => {
     if (err) {
         console.error(err);
@@ -23,13 +43,7 @@ fs.writeFileSync('day6node2.txt', 'Hello Write file syncronously');
 console.log("File written syncronously");
 console.log("Now reading the written text in file");
 
-fs.unlink('day6node2.txt', (err) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log("File deleted successfully");
-});
+
 
 fs.writeFile('day6node2.txt', 'Hello Write file asyncronously', (err) => {
     if (err) {
@@ -70,4 +84,11 @@ fs.writeFile('day6node2.txt', 'Hello Write file asyncronously again', (err) => {
         });
 
     }
+
+    fs.rename("day6node2.txt", "day6noderename.txt", (err) => {
+        if (err) {
+            console.log("error while renaming file");
+        }
+        console.log("file renamed to ${day6noderename.txt}");
+    });
 });
