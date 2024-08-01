54c35a007a4b1a5d4bad00109bf71769364aeae8
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-01 19:16:25 +0300

    delete and unlink

--- day6node1.js
+++ day6node1.js
@@ -22,3 +22,52 @@ fs.writeFile('day6node.txt', 'Hello Write file asyncronously', (err) => {
 fs.writeFileSync('day6node2.txt', 'Hello Write file syncronously');
 console.log("File written syncronously");
 console.log("Now reading the written text in file");
+
+fs.unlink('day6node2.txt', (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log("File deleted successfully");
+});
+
+fs.writeFile('day6node2.txt', 'Hello Write file asyncronously', (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    } else {
+        console.log("File written successfully");
+        console.log("Now reading the written text in file");
+        fs.readFile('day6node2.txt', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+
+            console.log(data);
+        });
+
+    }
+});
+
+fs.unlinkSync('day6node2.txt');
+console.log("File deleted syncronously");
+
+fs.writeFile('day6node2.txt', 'Hello Write file asyncronously again', (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    } else {
+        console.log("File written successfully");
+        console.log("Now reading the written text in file");
+        fs.readFile('day6node2.txt', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+
+            console.log(data);
+        });
+
+    }
+});
