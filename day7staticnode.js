d6ae92e63960db8d160d3b7d98b464d2f0516180
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-02 18:51:23 +0300

    add routing with fs

---
+++ day7staticnode.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const url = req.url;
+    if (url === '/about') {
+        fs.readFile('webview/about.html', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            res.write(data);
+            res.end();
+        });
+    } else if (url === '/contact') {
+        fs.readFile('webview/contact.html', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            res.write(data);
+            res.end();
+        });
+    } else {
+        res.write('${url} not found');
+        res.end();
+    }
+}).listen(3000, () => {
+    console.log("server start at port 3000");
+});
