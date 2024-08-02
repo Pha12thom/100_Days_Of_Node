14850e18d48b4104629e57aa4249387cb40b3d66
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-02 18:52:00 +0300

    comit routing

--- day7staticnode.js
+++ day7staticnode.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
+
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
