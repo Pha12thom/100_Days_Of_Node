0bbe177a513f5332ada8bf83b876b38bf5d68728
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-03 22:17:25 +0300

    express app route

--- day8expressapp.js
+++ day8expressapp.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.send('this is express routing');
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`app listening at http://localhost:${port}`);
 });
