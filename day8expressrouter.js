43442b31bf3ae5cbee6d83f6d02031da31c004d9
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-03 22:35:39 +0300

    push express

--- day8expressrouter.js
+++ day8expressrouter.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-
-router.get('/', (req, res, next) => {
-    res.send('this is express routing for homepage');
+router.get('/', (req, res) => {
+    res.send('This is express routing for the homepage');
 });
 
-router.get('/about', (req, res, next) => {
-    res.send('this is express routing for about page');
+router.get('/about', (req, res) => {
+    res.send('This is express routing for the about page');
 });
 
-router.get('/contact', (req, res, next) => {
-    res.send('this is express routing for contact page');
+router.get('/contact', (req, res) => {
+    res.send('This is express routing for the contact page');
 });
 
-module.export = router;
+module.exports = router;
