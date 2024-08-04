127fe467c049b103f337537b563e4819851028c8
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-04 17:11:35 +0300

    Update day9expressmiddleware.js

--- day9expressmiddleware.js
+++ day9expressmiddleware.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('welcome to express middlewares');
 });
 
 app.use((err, req, res, next) => {
