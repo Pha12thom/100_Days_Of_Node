3ee458c21de6c846545f7491c6a027013c759742
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-04 17:22:35 +0300

    push express middlewares

--- day9expressmiddleware.js
+++ day9expressmiddleware.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const router = express.Router();
 const app = express();
 
 
@@ -8,16 +9,28 @@ app.use((req, res, next) => {
     next();
 });
 
+
 app.use(express.json());
+
+
+app.use("/router", router);
+
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Internal Server Error');
 });
+
+
+router.get('/', (req, res) => {
+    res.send('Hello from the router!');
+});
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
