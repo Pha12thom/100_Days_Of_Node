f8c44a97ab2cdcc7df2fd3d846b2a63a49c673f4
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-05 23:34:34 +0300

    push the get and post file

---
+++ day10get_post.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(express.static(path.join(__dirname, 'webview')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'webview', 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'webview', 'login.html'));
+});
+
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if (username === 'user' && password === 'user') {
+        res.redirect(`/welcome?username=${username}`);
+    } else {
+        res.send('Invalid credentials');
+    }
+});
+
+
+app.get('/welcome', (req, res) => {
+    const { username } = req.query;
+    res.send(`Welcome, ${username}!`);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
