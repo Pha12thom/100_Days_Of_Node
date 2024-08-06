db19a3e8fc079f8f1ee443599171ddb4825bb745
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-06 19:03:40 +0300

    use static folder

--- day11serve_staticfiles.js
+++ day11serve_staticfiles.js
@@ -2,10 +2,9 @@ const express = require('express');
 
 const app = express();
 
-// Serve static files from the "webview" directory
+
 app.use(express.static('webview'));
 
-// Start the server
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
