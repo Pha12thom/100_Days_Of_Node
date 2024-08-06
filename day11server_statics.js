22dc350b3ea586256ef5678cadb04d464254fea0
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-06 22:53:04 +0300

    push image to form

--- day11server_statics.js
+++ day11server_statics.js
@@ -6,8 +6,10 @@ const path = require('path')
 app.use('/static', express.static(path.join(__dirname, 'webview')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'webview', 'about.html'));
+    res.sendFile(path.join(__dirname, 'webview', 'login.html'));
 });
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
