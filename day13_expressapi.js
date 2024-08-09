66b874e30afd266b83c7338120d5094e2471f062
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-09 18:17:42 +0300

    handle push product

--- day13_expressapi.js
+++ day13_expressapi.js
@@ -12,38 +12,40 @@ myproducts = [
 
 
 app.get('/products', (req, res) => {
+    res.json(myproducts);
+});
 
 
 
 
-            app.get('/products/:id', (req, res) => {
-                const productId = req.params.id;
-                res.json()
+app.get('/products/:id', (req, res) => {
+    const productId = req.params.id;
+    const product = myproducts[productId];
+    res.json(product);
 
 
 
-            });
+});
 
-            app.post('/products', (req, res) => {
-                const newProduct = req.body;
-                // Logic to create a new product in the database
-                // Return the created product as JSON response
-            });
+app.post('/products', (req, res) => {
+    const newProduct = req.body;
+    myproducts.push(newProduct);
+    res.json(newProduct);
+});
 
-            app.put('/products/:id', (req, res) => {
-                const productId = req.params.id;
-                const updatedProduct = req.body;
-                // Logic to update a specific product by ID in the database
-                // Return the updated product as JSON response
-            });
+app.put('/products/:id', (req, res) => {
+    const productId = req.params.id;
+    const updatedProduct = req.body;
 
-            app.delete('/products/:id', (req, res) => {
-                const productId = req.params.id;
-                // Logic to delete a specific product by ID from the database
-                // Return a success message as JSON response
-            });
+});
 
-            // Start the server
-            app.listen(PORT, () => {
-                console.log(`Server is running on port ${PORT}`);
-            });
+app.delete('/products/:id', (req, res) => {
+    const productId = req.params.id;
+    // Logic to delete a specific product by ID from the database
+    // Return a success message as JSON response
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
