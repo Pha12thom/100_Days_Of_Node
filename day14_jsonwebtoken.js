018c20bf4d4d91b0bfa64543de2f92b40cb6d6fe
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-10 22:41:22 +0300

    authenticate jwt

--- day14_jsonwebtoken.js
+++ day14_jsonwebtoken.js
@@ -2,9 +2,57 @@ const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
 
+const students = [{
+        id: 1,
+        name: 'milugo Geofrey',
+
+        age: 21,
+        course: 'Mathematics and CCS',
+    },
+    {
+        id: 2,
+        name: 'John Doe',
+        age: 24,
+        course: 'CCS',
+
+    }
+];
+
 console.log(require('crypto').randomBytes(64).toString('hex'));
 
 
 function generateAccessToken(username) {
-    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+    return jwt.sign(username, process.env.SECRET_TOKEN, { expiresIn: '1800s' });
+}
+
+
+app.post('/api/createNewUser', (req, res) => {
+
+
+    const token = generateAccessToken({ username: req.body.username });
+    res.json(token);
+
+
+});
+
+
+
+
+const jwt = require('jsonwebtoken');
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
+        console.log(err)
+
+        if (err) return res.sendStatus(403)
+
+        req.user = user
+
+        next()
+    })
 }
