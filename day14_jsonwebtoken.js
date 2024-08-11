d1e2208f569440ab70039d26493df5310cf52f71
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-11 19:33:03 +0300

    push jwt authentication and authorization

--- day14_jsonwebtoken.js
+++ day14_jsonwebtoken.js
@@ -1,31 +1,27 @@
 const jwt = require('jsonwebtoken');
 const express = require('express');
-const router = express.Router();
+const app = express();
+app.use(express.json()); // To parse JSON bodies
 
 const students = [{
         id: 1,
         name: 'milugo Geofrey',
-
         age: 21,
-        course: 'Mathematics and CCS',
+        course: 'Mathematics and CCS'
     },
     {
         id: 2,
         name: 'John Doe',
         age: 24,
-        course: 'CCS',
-
+        course: 'CCS'
     }
 ];
 
 console.log(require('crypto').randomBytes(64).toString('hex'));
 
-
 function generateAccessToken(username) {
-    const username = req.body.name;
     return jwt.sign(username, process.env.SECRET_TOKEN, { expiresIn: '1800s' });
 }
-
 
 app.post('/api/createNewUser', (req, res) => {
     const username = req.body.name;
@@ -33,29 +29,21 @@ app.post('/api/createNewUser', (req, res) => {
     res.json({ accessToken });
 });
 
-
-
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
+    const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.sendStatus(401)
+    if (token == null) return res.sendStatus(401);
 
-    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-        console.log(err)
-
-        if (err) return res.sendStatus(403)
-        req.user = user
-
-        next()
-    })
+    jwt.verify(token, process.env.SECRET_TOKEN, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
 }
-
-
 
 app.get('/api/protected', authenticateToken, (req, res) => {
     res.json({ message: `Hello, ${req.user.username}` });
 });
-
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
