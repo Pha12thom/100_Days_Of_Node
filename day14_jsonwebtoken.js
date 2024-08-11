add6854ae6619429e9c582539cded433f0ac3a4b
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-11 19:24:46 +0300

    jwt implementation

--- day14_jsonwebtoken.js
+++ day14_jsonwebtoken.js
@@ -49,3 +49,14 @@ function authenticateToken(req, res, next) {
         next()
     })
 }
+
+
+
+app.get('/api/protected', authenticateToken, (req, res) => {
+    res.json({ message: `Hello, ${req.user.username}` });
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
