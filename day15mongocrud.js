3e2b84936b9cb1ccf2c933b0edb4d0c4f61de759
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-13 17:36:52 +0300

    Update day15mongocrud.js

--- day15mongocrud.js
+++ day15mongocrud.js
@@ -1,47 +1,42 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
+const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
+const express = require('express');
+const router = express.Router();
+const app = express();
+
+
+const model = require('../models/usermodel');
+app.use('/', model);
+
 app.use(express.json());
 
-
-const studentSchema = new mongoose.Schema({
-    username: String,
-    name: String,
-    age: Number,
-    email: String,
-    password: String,
-
-});
+console.log(crypto.randomBytes(64).toString('hex'));
 
 
-const Student = mongoose.model('school', studentSchema);
-
-mongoose.connect('mongodb://localhost:27017/students').then(() => {
-    console.log('Connected to MongoDB');
-}).catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-});
+function generateToken(user) {
+    return jwt.sign({ data: user }, 'secret', { expiresIn: '1h' });
+}
 
 
-app.post('/students', async(req, res) => {
-    try {
-        const student = new Student(req.body);
-        const savedStudent = await student.save();
-        res.status(201).send(savedStudent);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
+function authenticateToken(req, res, next) {
+    const token = req.headers['authorization'].split(' ')[1];
+    if (!token)
+        return res.status(401).json({ message: 'Token not provided' });
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Token is invalid' });
+        req.user = user;
+        next();
+    });
+}
 
-app.post('/login', async(req, res) => {
+const login = router.post('/login', async(req, res) => {
     const { username, password } = req.body;
     if (!username && !password) {
         return res.status(400).json({ error: 'All fields are required' });
     } else {
-        const student = await Student.findOne({ username, password });
-        if (student) {
-            res.status(200).json(student);
+        const user = await model.findOne({ username, password });
+        if (user) {
+            res.status(200).json({ token: generateToken(user) });
         } else {
             res.status(400).json({ error: 'Invalid credentials' });
         }
@@ -49,26 +44,20 @@ app.post('/login', async(req, res) => {
 });
 
 
-app.post('/register', async(req, res) => {
+const register = router.post('/register', async(req, res) => {
     const { username, name, email, password, age } = req.body;
     if (!username && !name && !email && !password && !age) {
         return res.status(400).json({ error: 'All fields are required' });
     } else {
-        const student = new Student(req.body);
-        const savedStudent = await student.save();
-        res.status(201).send(savedStudent);
-    }
-})
-
-app.get('/students', async(req, res) => {
-    try {
-        const students = await Student.find();
-        res.status(200).json(students);
-    } catch (error) {
-        res.status(500).json({ error: 'An error occurred while fetching students' });
+        const user = new model(req.body);
+        const savedUser = await user.save();
+        res.status(201).send(savedUser);
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+
+
+
+
+module.exports = register;
+module.exports = login;
