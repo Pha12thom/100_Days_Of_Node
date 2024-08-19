7b3cab55e90f2f04c776cfac0704107614af2468
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-19 19:12:02 +0300

    push model

---
+++ day21_30/controllers/controller.js
@@ -0,0 +1,51 @@
+import User from "../models/usermodel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { v4 as uuidv4 } from "uuid";
+import dotenv from "dotenv";
+dotenv.config();
+
+const register = async(req, res) => {
+    const { name, age, email, password } = req.body;
+    try {
+        const userExist = await User.findOne({ email, name });
+        if (userExist) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ name, age, email, password: hashedPassword });
+        jwt.sign({ email }, process.env.SECRET_TOKEN, { expiresIn: "1h" });
+        const savedUser = await newUser.save();
+        return res.status(200).json(savedUser);
+
+    } catch (error) {
+        console.error("Error during registration:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+
+const login = async(req, res) => {
+    const { email, password } = req.body;
+    try {
+        const userExist = await User.findOne({ email, password });
+        if (!userExist) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+        const validPassword = await bcrypt.compare(password, userExist.password);
+        if (validPassword) {
+            const token = jwt.sign({ email }, process.env.SECRET_TOKEN, { expiresIn: "1h" });
+            const sessionId = uuidv4();
+            res.cookie("sessionId", sessionId, { httpOnly: true });
+            res.cookie("token", token, { httpOnly: true });
+            return res.status(200).json({ message: "Logged in successfully", token });
+        } else {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+    } catch (error) {
+        console.error("Error during login:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export { register, login };
---
+++ day21_30/models/usermodel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
