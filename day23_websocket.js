e0523ec7a1789a5399d3df661c36948a6015c5e1
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-21 19:25:50 +0300

    wss

--- day23_websocket.js
+++ day23_websocket.js
@@ -1,18 +1,22 @@
-import express from "express";
-import http from "http";
-import path from "path";
-import { fileURLToPath } from "url";
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
+const port = 3000;
 const app = express();
-const PORT = 3000;
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server })
 
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        })
+    })
+})
 
-const __filename = fileURLToPath(
-    import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+server.listen(port, function() {
+    console.log(`Server is listening on ${port}!`)
+})
