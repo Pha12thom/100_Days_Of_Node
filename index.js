3be64c1003501124e1d8a34ffc6fb6e9bef06ba4
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-22 23:07:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Worldf ofbexpress js!'))
 app.listen(3000, () => console.log('Server ready'))
