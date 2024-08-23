1ce75d6dd6c7ecc6b37f090490e45b908fefe9a4
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-23 23:17:40 +0300

    cron task invoice

---
+++ day21_30/day24_cron.js
@@ -0,0 +1,20 @@
+import cron from "node-cron";
+import fs from "fs";
+import path from "path";
+import invoices from "../data/invoice.json";
+
+const archiveInvoice = () => {
+    console.log("invoice tasks running", new Date());
+
+
+    const filteredInvoices = invoices.filter((invoice) => invoice.status === "sold");
+
+    // filtered invoices to file
+    const filteredInvoicesPath = path.join(__dirname, "data", "myfiltered-invoice.json");
+    fs.writeFileSync(filteredInvoicesPath, JSON.stringify(filteredInvoices));
+
+    console.log("Filtered invoices have been archived.");
+};
+
+// Schedule the cron task to run every day at 12:00 PM
+cron.schedule("0 12 * * *", archiveInvoice);
