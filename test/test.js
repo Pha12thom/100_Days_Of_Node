17ee62b9f62137a07f1292bc7c2c9ce6e2abf5c3
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-25 22:14:56 +0300

    CRlF add

---
+++ test/test.js
@@ -0,0 +1,16 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+function add(a, b) {
+    return a + b;
+}
+
+describe('Addition', function() {
+    it('should return 4 when adding 2 and 2', function() {
+        expect(add(2, 2)).to.equal(4);
+    });
+
+    it('should return 0 when adding 2 and -2', function() {
+        expect(add(2, -2)).to.equal(0);
+    });
+});
