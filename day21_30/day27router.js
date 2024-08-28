42f21f0750dd90a56dd2310a924037f45900a65a
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-08-28 23:57:54 +0300

    routers db

--- day21_30/day27router.js
+++ day21_30/day27router.js
@@ -1,8 +1,16 @@
+import mongoose from 'mongoose';
 import router from './day27_expressRouter.js';
 import express from 'express';
 
 const app = express();
 app.use('/api', router);
+
+
+mongo.connect('mongodb://localhost:27017/day27').then(() => {
+    console.log('Connected to database');
+}).catch((err) => {
+    console.log(err);
+});
 
 
 app.listen(3000, () => {
