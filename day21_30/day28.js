b68bc7431e0e893746a2589a25d0ee510f34ebca
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-09-01 22:21:36 +0300

    add day 28

---
+++ day21_30/day28.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import { login } from './controllers/controller.js';
+import User from './models/usermodel.js';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const router = express.Router();
+
+
+
+function authenticateToken(req, res, next) {
+    const token = req.headers['authorization'].split(' ')[1];
+    if (!token)
+        return res.status(401).json({ message: 'Token not provided' });
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Token is invalid' });
+        req.user = user;
+        next();
+    });
+}
+
+app.post()
