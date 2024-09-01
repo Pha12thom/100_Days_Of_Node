a510513ccd3aaf01a46d895a80fded76e8eab09d
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-09-01 22:47:23 +0300

    setup swagger ui

--- day21_30/day29_swagger.js
+++ day21_30/day29_swagger.js
@@ -1,2 +1,24 @@
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+import express from 'express';
+
+const app = express();
+app.use(express.json())
+
+const swaggerOptions = {
+    definition: {
+        info: {
+            title: 'swagger User API',
+            description: 'User API Information',
+            contact: {
+                name: 'Amazing Developer'
+            },
+            servers: ['http://localhost:3000']
+        }
+    },
+    apis: ['./day29_swagger.js']
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
