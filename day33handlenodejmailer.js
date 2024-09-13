825488bf388ccc9aa063bcb3703083e879bb9daf
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-09-13 08:02:51 +0300

    add nodemailer sent

--- day33handlenodejmailer.js
+++ day33handlenodejmailer.js
@@ -4,16 +4,24 @@ import express from "express";
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: process.env.GMAIL,
-        pass: process.env.PASS,
+        user: 'geofreymilugo@gmail.com',
+        pass: 'nrmxixzytslzowit',
     }
 
 });
 
 
 const mailOptions = {
-    from: 'milugogeogrey@gmail.com',
-    to: 'geofreymilugo@gmail.com',
+    from: 'geofreymilugo@gmail.com',
+    to: 'milugogeogrey@gmail.com',
     subject: 'This is Test Mailer',
     text: 'Hello, this is a test email sent using Nodemailer from milugogeogrey@gmail.com'
 };
+
+
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Email sent: ' + info.response);
+});
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hello Worldf ofbexpress js!'))
-app.listen(3000, () => console.log('Server ready'))
+app.get('/', (req, res) => res.send('Hello World of express js!'))
+app.listen(3000, () => console.log('Server ready'));
